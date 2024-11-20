faecf28f6627a596a5629d78e0033b773260816d
Roman Maksimov <466600@niuitmo.ru>
2024-11-20 19:50:35 +0300

    fix function finding triangle's area (Heron's formula) and cal function. Also add .gitignore file

--- calculate.py
+++ calculate.py
@@ -1,17 +1,26 @@
 import circle
 import square
+import triangle
 
 
-figs = ['circle', 'square']
+figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
-sizes = {}
+sizes = {
+	"area-circle" : 1,
+	"area-square" : 1,
+	"area-triangle" : 3,
+
+	"perimeter-circle" : 1,
+	"perimeter-square" : 1,
+	"perimeter-triangle" : 3,
+}
 
 def calc(fig, func, size):
 	assert fig in figs
 	assert func in funcs
 
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
+	return eval(f'{fig}.{func}(*{size})')
+
 
 if __name__ == "__main__":
 	func = ''
@@ -19,15 +28,16 @@ if __name__ == "__main__":
 	size = list()
     
 	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
+		fig = input(f"Enter figure name, available are {figs}:\n")
 	
 	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
+		func = input(f"Enter function name, available are {funcs}:\n")
 	
 	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
+		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square, 3 for triangle\n").split()))
 	
-	calc(fig, func, size)
+	result = calc(fig, func, size)
+	print(result)
 
 
 
